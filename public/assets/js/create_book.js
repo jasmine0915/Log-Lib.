cd37125cd028fff1f951106bef1f459951a449f0
kagero0915 <kagero0915@gmail.com>
2016-06-18 07:05:51 +0000

    little

--- public/assets/js/create_book.js
+++ public/assets/js/create_book.js
@@ -1,6 +1,6 @@
 $(function(){
   //***create_book***
   $("#url_add").click(function(){
-    $("#contents").prepend("<label for='url'>URL :</label><br><input type='url' name='url[]'><br><label for='url_title'>URL TILTE :</label><br><input type='title' name='url_title[]'><br><br>");
+    $("#contents").prepend("<label for='url'>URL :</label><br><input type='url' name='url[]'><br><label for='url_title'>URL TILTE :</label><br><input type='title' name='url_title[]'><br><br><label for='content'>CONTENT :</label><br><textarea name='content[]' rows=4 cols=60></textarea><br>");
   });
 });
